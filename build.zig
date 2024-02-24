74f718bd2021c27a2c6256e42e614bccbfb5f4a8
Rahul Badgujar <rahulbadgujar1508@gmail.com>
2024-02-24 21:05:19 +0530

    add basic zap based http server

--- build.zig
+++ build.zig
@@ -5,5 +5,16 @@ pub fn build(b: *std.Build) void {
         .name = "main",
         .root_source_file = .{ .path = "main.zig" },
     });
+
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const zap = b.dependency("zap", .{
+        .target = target,
+        .optimize = optimize,
+        .openssl = false, // set to true to enable TLS support
+    });
+    exe.addModule("zap", zap.module("zap"));
+    exe.linkLibrary(zap.artifact("facil.io"));
+
     b.installArtifact(exe);
 }
--- main.zig
+++ main.zig
@@ -1,7 +1,30 @@
 const std = @import("std");
+const zap = @import("zap");
 
-pub fn main() void {
-    std.debug.print("Hello, Bert\n", .{});
-    const name: []const u8 = "Rahul";
-    std.debug.print("Hello, {s}\n", .{name});
+fn on_request(r: zap.Request) void {
+    if (r.path) |the_path| {
+        std.debug.print("PATH: {s}\n", .{the_path});
+    }
+
+    if (r.query) |the_query| {
+        std.debug.print("QUERY: {s}\n", .{the_query});
+    }
+    r.sendBody("<html><body><h1>Hello from Tenfins!!!</h1></body></html>") catch return;
+}
+
+pub fn main() !void {
+    var listener = zap.HttpListener.init(.{
+        .port = 3000,
+        .on_request = on_request,
+        .log = true,
+    });
+    try listener.listen();
+
+    std.debug.print("Listening on localhost:3000\n", .{});
+
+    // start worker threads
+    zap.start(.{
+        .threads = 2,
+        .workers = 2,
+    });
 }
