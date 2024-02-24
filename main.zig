55ffe8be6f47185eb830ea510f3fd98babc117e9
Rahul Badgujar <rahulbadgujar1508@gmail.com>
2024-02-24 20:42:16 +0530

    update vscode config for linux

--- main.zig
+++ main.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn main() void {
-    std.debug.print("Hello, Bert", .{});
+    std.debug.print("Hello, Bert\n", .{});
     const name: []const u8 = "Rahul";
-    std.debug.print("Hello, {s}", .{name});
+    std.debug.print("Hello, {s}\n", .{name});
 }
